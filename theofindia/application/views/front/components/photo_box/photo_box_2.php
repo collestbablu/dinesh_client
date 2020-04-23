5fd119e3381a28aa4e429c737d02d906bae2b2d9
collestbablu <collestbablu@gmail.com>
2020-04-23 15:58:06 +0530

    remove page loader script from photo gallery

--- theofindia/application/views/front/components/photo_box/photo_box_2.php
+++ theofindia/application/views/front/components/photo_box/photo_box_2.php
@@ -1,34 +1,71 @@
-<?php 
-    $imgs = json_decode($img_features,true);
-    foreach ($imgs as $roq) {
-        if($roq['index'] == '0'){
-            $img = $roq['img'];
-        }
-    }
-    $image = base_url().'uploads/photo_image/'.$img;
-?>
-<div class="thumbnail photo_box_2">
-    <div class="media">
-        <div class="media-object img-responsive image_delay" data-src="<?php echo $image; ?>" style="height:300px; border: 2px solid #eaeaea;
-    box-shadow: 1px 1px 6px rgba(0,0,0,.3); border-radius: 8px; background-image: url('<?php echo img_loading();?>');background-repeat: no-repeat; background-position: center; background-size: cover;"></div>
-        <div class="caption hovered">
-            <div class="caption-wrapper div-table">
-                <div class="caption-inner div-cell">
-                    <p class="caption-buttons hidden-xs">
-                        <span class="btn caption-zoom" onClick="image_modal('<?php echo $image; ?>');">
-                            <i class="fa fa-search"></i>
-                        </span>
-                        <a href="<?php echo $this->Crud_model->photo_link($photo_id); ?>" class="btn caption-link"><i class="fa fa-link"></i></a>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="caption title">
-        <h3 class="caption-title">
-            <a href="<?php echo $this->Crud_model->photo_link($photo_id); ?>">
-                <?php echo word_limiter($title, 5); ?>
-            </a>
-        </h3>
-    </div>
-</div>
+<?php 
+
+    $imgs = json_decode($img_features,true);
+
+    foreach ($imgs as $roq) {
+
+        if($roq['index'] == '0'){
+
+            $img = $roq['img'];
+
+        }
+
+    }
+
+    $image = base_url().'uploads/photo_image/'.$img;
+
+?>
+
+<div class="thumbnail photo_box_2">
+
+    <div class="media">
+
+        <!-- <div class="media-object img-responsive image_delay" data-src="<?php echo $image; ?>" style="height:300px; border: 2px solid #eaeaea;
+
+    box-shadow: 1px 1px 6px rgba(0,0,0,.3); border-radius: 8px; background-image: url('<?php echo img_loading();?>');background-repeat: no-repeat; background-position: center; background-size: cover;"></div> -->
+    <div class="media-object img-responsive image_delay1"  style="height:300px; border: 2px solid #eaeaea;
+
+    box-shadow: 1px 1px 6px rgba(0,0,0,.3); border-radius: 8px; background-image: url(<?php echo $image; ?>);background-repeat: no-repeat; background-position: center; background-size: cover;"></div>
+
+        <div class="caption hovered">
+
+            <div class="caption-wrapper div-table">
+
+                <div class="caption-inner div-cell">
+
+                    <p class="caption-buttons hidden-xs">
+
+                        <span class="btn caption-zoom" onClick="image_modal('<?php echo $image; ?>');">
+
+                            <i class="fa fa-search"></i>
+
+                        </span>
+
+                        <a href="<?php echo $this->Crud_model->photo_link($photo_id); ?>" class="btn caption-link"><i class="fa fa-link"></i></a>
+
+                    </p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="caption title">
+
+        <h3 class="caption-title">
+
+            <a href="<?php echo $this->Crud_model->photo_link($photo_id); ?>">
+
+                <?php echo word_limiter($title, 5); ?>
+
+            </a>
+
+        </h3>
+
+    </div>
+
+</div>
+
